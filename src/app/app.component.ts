8d87212a2c045c260a7ca88d68e3a008381b2a85
Dmitry Vasilevich <wotlk777@gmail.com>
2019-09-18 00:39:09 +0300

    some directives magic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,33 @@ export class AppComponent {
   userName = "";
   alertName = "";
   alertCreationStatus = "no alert was created";
-  allowNewServer = false;
+  isAlertCreated = false;
+  alerts = ["Test server", "Test server 2"];
 
+  allowNewServer = false; // example
+
+  displayStatus = false;
+  displayStatusLogs = [];
+
+  // as an example of property binding
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
-  clearUserName() {
-    this.userName = "";
+  createAlert() {
+    this.isAlertCreated = true;
+    this.alerts.push(this.alertName);
   }
 
+  // as an example of event binding
   onUserNameChange(event: Event) {
     this.userName = (event.target as HTMLInputElement).value;
+  }
+
+  displayDetails() {
+    this.displayStatus = !this.displayStatus;
+    this.displayStatusLogs.push(`${Date.now().toLocaleString("ru-RU")} - ${this.displayStatus ? "shown" : "hidden"}`);
   }
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,14 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./server.component.sass"]
 })
 export class ServerComponent implements OnInit {
+  serverName = "";
   serverStatus = "offline";
+  servers = ["Test server", "Test server 2"];
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() >= 0.5 ? "online" : "offline";
+  }
 
   ngOnInit() {
   }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  onCreateServer() {
+   this.servers.push(this.serverName);
+  }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
